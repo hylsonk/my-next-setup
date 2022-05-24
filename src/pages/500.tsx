0a16a4d2cbfcc476860d57149c88236443df7dcb
Hylsonk <hylsonk@gmail.com>
2022-05-23 23:56:02 -0300

    Error handling

---
+++ src/pages/500.tsx
@@ -0,0 +1,15 @@
+import useTranslation from "next-translate/useTranslation"
+import React from "react"
+import { Heading } from "../components/ui/atoms"
+
+const Custom500 = (): JSX.Element => {
+  const { t } = useTranslation()
+
+  return (
+    <div>
+      <Heading headingLevel="h1">{t("error:500")}</Heading>
+    </div>
+  )
+}
+
+export default Custom500
